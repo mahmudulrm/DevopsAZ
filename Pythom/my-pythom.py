3c5194405fa61dfddca9bbcc7d13ccf8e3454b70
mahmudulrm <mahmudulrm@gmail.com>
2023-09-02 02:32:16 +0600

    new file adding

--- Pythom/my-pythom.py
+++ Pythom/my-pythom.py
@@ -1,2 +1,10 @@
 print("20 days are " + str(20) + " minutes")
 print(f"20 days are {20 * 24 * 60} minutes")
+
+#veriables
+calculattion_to_minutes = 24 * 60
+name_of_unit = "minutes"
+
+print(f"20 days are {20 * calculattion_to_minutes} {name_of_unit}")
+print(f"25 days are {25 * calculattion_to_minutes} {name_of_unit}")
+print(f"30 days are {30 * calculattion_to_minutes} {name_of_unit}")
