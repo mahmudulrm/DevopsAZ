952c1f85107fb3da9c9927f2c7d7989dd082b885
mahmudulrm <mahmudulrm@gmail.com>
2023-09-02 02:52:45 +0600

    new file adding

--- Pythom/my-pythom.py
+++ Pythom/my-pythom.py
@@ -10,10 +10,21 @@ print(f"25 days are {25 * calculattion_to_minutes} {name_of_unit}")
 print(f"30 days are {30 * calculattion_to_minutes} {name_of_unit}")
 
 #Function
-def days_to_units(number_of_days):
+def days_to_units(number_of_days, custom_message):
     print(f"{number_of_days} days are {number_of_days * calculattion_to_minutes} {name_of_unit}")
-    print("Function is working")
+    print(f"Function is working {custom_message} ")
 
 
-days_to_units(30)
-days_to_units(100)
+days_to_units(30, "GOOD")
+days_to_units(100, "Awesome")
+days_to_units(30, "okay")
+
+#Scope variable and function
+
+def scope_check(number_of_days):
+    my_var = "Fuction Variable"
+    print(number_of_days)
+    print(my_var)
+
+
+scope_check(20)
