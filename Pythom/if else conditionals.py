2a5a0800732f00706c9eef238f0a7325627a029d
mahmudulrm <mahmudulrm@gmail.com>
2023-09-04 16:51:16 +0600

    new file adding

--- Pythom/if else conditionals.py
+++ Pythom/if else conditionals.py
@@ -3,9 +3,11 @@ calculattion_to_minutes = 24 * 60
 name_of_unit = "minutes"
 
 def days_to_units(number_of_days):
+    print(number_of_days > 0)
     if number_of_days > 0:
         return f"{number_of_days} days are {number_of_days * calculattion_to_minutes} {name_of_unit}"
     else:
+        print(number_of_days > 0)
         return "you enter a negative value, so no conversion for you!"
 
 
