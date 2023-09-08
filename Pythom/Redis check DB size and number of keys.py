fbd3d4815fd73353da0ac4884c27d9e8f4004131
mahmudulrm <mahmudulrm@gmail.com>
2023-09-08 22:28:30 +0600

    new file adding

--- Pythom/Redis check DB size and number of keys.py
+++ Pythom/Redis check DB size and number of keys.py
@@ -1,7 +1,7 @@
 #Redis check DB size and number of keys
 import redis
-servers = ["10.0.3.121", "10.0.3.122", "10.0.3.123"]
-password = 'tz2m9uqdelgh7*4'
+servers = ["10.0.3.12", "10.0.3.12", "10.0.3.12"]
+password = 'z2mdff'
 for server in servers:
     try:
         r = redis.StrictRedis(host=server, port=6379, password=password)
@@ -9,3 +9,22 @@ for server in servers:
         print(f"Total number of keys on {server}: {num_keys}")
     except Exception as e:
         print(f"Failed to connect to {server}: {str(e)}")
+
+#-----
+#import redis
+
+password = "z2mdff"
+servers = [{"host": "10.0.3.12", "port": 6379},
+           {"host": "10.0.3.12", "port": 6379},
+           {"host": "10.0.3.12", "port": 6379}]
+
+for server in servers:
+    try:
+        r = redis.StrictRedis(host=server['host'], port=server['port'], password=password)
+        role_info = r.info('replication')
+        if role_info['role'] == 'master':
+            print(f"Master node of {server['host']}:{server['port']} is {server['host']}:{server['port']}")
+        else:
+            print(f"{server['host']}:{server['port']} is not a master node")
+    except redis.exceptions.ConnectionError:
+        print(f"Cannot connect to {server['host']}:{server['port']}")
